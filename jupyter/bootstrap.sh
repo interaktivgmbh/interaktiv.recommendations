6c5cf9a8d0d82d5570f0ba5996f3dddafe872fbd
rbraun <braun@interaktiv.de>
2022-10-14 17:33:18 +0200

    Removed presentation from repository.

--- jupyter/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-[ ! -d "./.venv/" ] && virtualenv -p python3.8 ./.venv/
-./.venv/bin/pip install -r ./requirements.txt
--- jupyter/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-.venv/bin/jupyter-notebook RecommendationsTalk.ipynb
